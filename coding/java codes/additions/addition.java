599d3eeb81ae3281f5891072a8185ba586faec67
Yashwanth Sure <142418837+YashwanthSure@users.noreply.github.com>
2024-08-12 10:48:08 +0530

    Add files via upload
    combined projects

---
+++ coding/java codes/additions/addition.java
@@ -0,0 +1,12 @@
+package additions;
+
+public class addition {
+    public static void mian(String args[])
+    {
+        int a=50;
+        int b=60;
+        int sum=a+b;
+        System.out.println("addition of two numbers is"+sum);
+    }
+
+}
---
+++ coding/java codes/additions/age.java
@@ -0,0 +1,21 @@
+package additions;
+import java.util.*;
+public class age {
+    public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    int yash=sc.nextInt();
+    int narendra=sc.nextInt();
+    System.out.println("age calculation");
+    if(narendra>yash)
+    {
+        System.out.println("elder");
+    }
+    else
+    {
+        System.out.println("younger");
+    }
+    sc.close();
+
+    }
+    
+}
